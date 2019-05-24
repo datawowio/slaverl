c9060c8fe8fa02ad67e517ce3ef58bd13f3c0fa3
Jeep Kiddee <nutthawut.ki@gmail.com>
2019-05-24 15:51:23 +0700

    Add player id and some statuses

--- components/player.py
+++ components/player.py
@@ -1,11 +1,16 @@
 class Player:
-    def __init__(self):
+    def __init__(self, player_id=None):
         self.cards = []
         self.won_place = None
         self.title = None
+        self.player_id = player_id
+        self.is_passing = False
 
     def draw_card(self, deck):
         self.cards.append(deck.draw())
 
-    def is_finished(self):
+    def has_won(self):
         return len(self.cards) == 0
+
+    def __repr__(self):
+        return f"player[id={self.player_id}]"
--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from components.player import Player
 from components.table import Table
 
 
-table = Table(players=[Player() for _ in range(4)])
+table = Table(players=[Player(player_id=i) for i in range(4)])
 deck = Deck().shuffle()
 
 while not deck.is_empty():
